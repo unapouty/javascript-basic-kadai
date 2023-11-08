44287a1e8e3ee10a3dcfb5b19e7fb6d687f54900
unapouty <142999330+unapouty@users.noreply.github.com>
2023-11-08 12:48:51 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,9 @@
+// btnというidを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById('btn');
+ 
+setTimeout(() => {
+  text.textContent = 'ボタンをクリックされました';
+  btn.addEventListener('click',() => {
+    
+})
+}, 2000);
